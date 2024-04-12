6a32cb7bbf57b061035bc9e00487c77166725fd8
mark <mark.franke@icloud.com>
2024-04-12 20:31:40 +0100

    self.player

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 import sys
 import pygame
 
-
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
 
 
 class Game:
@@ -28,9 +28,13 @@ class Game:
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
+        self.tilemap = Tilemap(self, tile_size=16)
+
     def run(self):
         while True:
             self.display.fill((14, 219, 248))
+
+            self.tilemap.render(self.display)
 
             self.player.update((self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,18 +1,23 @@
-import elf
 import self
 
 
+
 class Tilemap:
-    def _init__(self, tile_size=16):
-        elf.tile_size = tile_size
-        elf.tilemap = {}
-        elf.offgrid_tiles = []
+    def _init__(self, game, tile_size=16):
+        self.game = game
+        self.tile_size = tile_size
+        self.tilemap = {}
+        self.offgrid_tiles = []
 
         for i in range(10):
             self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
             self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10 + i, 5)}
 
     def    render(self, surf):
+
+        for tile in self.offgrid_tiles:
+            surf.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'])
+
         for loc in self.tilemap:
             tile = self.tilemap[loc]
             surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size))
